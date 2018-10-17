6c4d7a122d70105aa8725f2411fe00ec26abaa06
namsukim <niceguy0825@naver.com>
2018-10-17 17:24:15 +0900

    binding is added.

--- app/src/main/java/com/test/english/ui/fragmentmusic/MusicFragment.java
+++ app/src/main/java/com/test/english/ui/fragmentmusic/MusicFragment.java
@@ -1,23 +1,27 @@
 package com.test.english.ui.fragmentmusic;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.exam.english.R;
+import com.exam.english.databinding.FragmentMusicBinding;
 
 public class MusicFragment extends Fragment {
+
+    private FragmentMusicBinding bingding;
 
     public static MusicFragment newInstance() {
         return new MusicFragment();
     }
 
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        //View view = inflater.inflate(R.layout.fragment_music, container, false);
-        return null;
+        bingding = DataBindingUtil.inflate(inflater, R.layout.fragment_music, container, false);
+        return bingding.getRoot();
     }
 }
