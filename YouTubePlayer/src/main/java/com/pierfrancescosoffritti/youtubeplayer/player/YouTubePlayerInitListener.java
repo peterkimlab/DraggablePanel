68937c02279806de1964ba7c357e5a30e4a7340d
peterkimlab <niceguy0825@hotmail.com>
2018-10-19 22:34:54 +0900

    YouTubePlayer is added.

---
+++ YouTubePlayer/src/main/java/com/pierfrancescosoffritti/youtubeplayer/player/YouTubePlayerInitListener.java
@@ -0,0 +1,7 @@
+package com.pierfrancescosoffritti.youtubeplayer.player;
+
+import android.support.annotation.NonNull;
+
+public interface YouTubePlayerInitListener {
+    void onInitSuccess(@NonNull YouTubePlayer youTubePlayer);
+}
