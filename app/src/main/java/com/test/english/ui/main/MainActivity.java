c1440e7dea1fe5ed2e9664a13128947e84ef5fcc
peterkimlab <niceguy0825@hotmail.com>
2018-10-19 21:59:12 +0900

    DraggablePanel is added.

--- app/src/main/java/com/test/english/ui/main/MainActivity.java
+++ app/src/main/java/com/test/english/ui/main/MainActivity.java
@@ -2,31 +2,21 @@ package com.test.english.ui.main;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-
 import com.exam.english.R;
 import com.exam.english.databinding.ActivityMainBinding;
 import com.test.english.api.Datums;
 import com.test.english.application.MyCustomApplication;
 import com.test.english.ui.adapter.MainViewPagerAdapter;
 import com.test.english.ui.frag1.Fragment1;
-import com.test.english.ui.fragmentmusic.MusicFragment;
 import com.test.english.ui.frag3.Fragment3;
 import com.test.english.ui.frag4.Fragment4;
+import com.test.english.ui.fragmentmusic.MusicFragment;
 import com.test.english.ui.helper.BottomNavigationNotShiftHelper;
-import com.test.english.util.HummingUtils;
-
-import java.math.BigDecimal;
-import java.util.List;
-
-import static com.test.english.application.MyCustomApplication.getApplicationInstance;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private MainViewPagerAdapter mainViewPagerAdapter;
 
     private int isSpeakLo = 0;
+
+    //private DraggablePanel draggableView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +76,59 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /*public void setVideoUrl(final Datums datums) {
+
+        if (getApplicationInstance().getConfig().getVideoMode().equals("youtube")) {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override public void run() {
+                    setYoutubeId(datums.source.get(HummingUtils.ElasticField.YOUTUBE_ID).toString(), 2000);
+                }
+            }, 10);
+        } else {
+            draggableView.setVisibility(View.VISIBLE);
+            draggableView.minimize();
+
+            // 재생 속도 초기화 : 100
+            int initSpeed = 100;
+            getVideoListFragment().playSpeedButtonInit(initSpeed);
+            BigDecimal pSpeed = new BigDecimal(initSpeed);
+            pSpeed = pSpeed.divide(new BigDecimal(100), 1, BigDecimal.ROUND_UP);
+            videoFragment.setPlaySpeed(pSpeed.floatValue());
+
+            // video Url 설정
+            videoFragment.setUrl(HummingUtils.IMAGE_PATH + datums.source.get(HummingUtils.ElasticField.VIDEO_URL), datums.source.get(HummingUtils.ElasticField.IDS).toString(), datums.source.get(HummingUtils.ElasticField.YOUTUBE_ID).toString());
+            // text 설정
+            videoFragment.setAlltext(datums.source.get(HummingUtils.ElasticField.TEXT_EN).toString(), datums.source.get(HummingUtils.ElasticField.TEXT_LO).toString(), "");
+            videoListFragment.initPlayer();
+            videoFragment.setBaseSentence(datums);
+            MainActivity.SEARCH_POPUP_VALUE = datums.source.get(HummingUtils.ElasticField.TEXT_EN).toString();
+            MainActivity.SEARCH_IDS_VALUE = datums.source.get(HummingUtils.ElasticField.IDS).toString();
+            MainActivity.SEARCH_YOUTUBE_VALUE = datums.source.get(HummingUtils.ElasticField.YOUTUBE_ID).toString();
+            MainActivity.SEARCH_YOUTUBE_CHANNEL_VALUE = datums.source.get(HummingUtils.ElasticField.YOUTUBE_CHANNEL_ID).toString();
+            MainActivity.SEARCH_CHECK = true;
+
+            // 썸네일 설정
+            List<String> thumbnails = (List<String>) datums.source.get(HummingUtils.ElasticField.THUMBNAILS);
+            videoListFragment.setThumbnails(thumbnails);
+            changeIds = true;
+
+            Handler handlers2 = new Handler();
+            handlers2.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (draggableView.isMaximized()) {
+                        if (changeIds) {
+                            changeIds = false;
+                            videoListFragment.changeView();
+                        }
+                    }
+                    postClick(MainActivity.SEARCH_IDS_VALUE);
+                }
+            }, 100);
+        }
+    }*/
+
     public void setupViewPager(ViewPager viewPager) {
         mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager());
 
@@ -117,63 +162,6 @@ public class MainActivity extends AppCompatActivity {
         return isSpeakLo;
     }
 
-    public void setVideoUrl(final Datums datums) {
-
-        /*if (getApplicationInstance().getConfig().getVideoMode().equals("youtube")) {
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override public void run() {
-                    setYoutubeId(datums.source.get(HummingUtils.ElasticField.YOUTUBE_ID).toString(), 2000);
-                }
-            }, 10);
-        } else {
-            draggableView.setVisibility(View.VISIBLE);
-            draggableView.minimize();
-
-            // 재생 속도 초기화 : 100
-            int initSpeed = 100;
-            getVideoListFragment().playSpeedButtonInit(initSpeed);
-            BigDecimal pSpeed = new BigDecimal(initSpeed);
-            pSpeed = pSpeed.divide(new BigDecimal(100), 1, BigDecimal.ROUND_UP);
-            videoFragment.setPlaySpeed(pSpeed.floatValue());
-
-            // video Url 설정
-            videoFragment.setUrl(HummingUtils.IMAGE_PATH + datums.source.get(HummingUtils.ElasticField.VIDEO_URL), datums.source.get(HummingUtils.ElasticField.IDS).toString(), datums.source.get(HummingUtils.ElasticField.YOUTUBE_ID).toString());
-            // text 설정
-            videoFragment.setAlltext(datums.source.get(HummingUtils.ElasticField.TEXT_EN).toString(), datums.source.get(HummingUtils.ElasticField.TEXT_LO).toString(), "");
-            videoListFragment.initPlayer();
-            videoFragment.setBaseSentence(datums);
-            MainActivity.SEARCH_POPUP_VALUE = datums.source.get(HummingUtils.ElasticField.TEXT_EN).toString();
-            MainActivity.SEARCH_IDS_VALUE = datums.source.get(HummingUtils.ElasticField.IDS).toString();
-            MainActivity.SEARCH_YOUTUBE_VALUE = datums.source.get(HummingUtils.ElasticField.YOUTUBE_ID).toString();
-            MainActivity.SEARCH_YOUTUBE_CHANNEL_VALUE = datums.source.get(HummingUtils.ElasticField.YOUTUBE_CHANNEL_ID).toString();
-            Log.e("test", "============================");
-            Log.e("test", "============================"+MainActivity.SEARCH_YOUTUBE_CHANNEL_VALUE);
-            MainActivity.SEARCH_CHECK = true;
-
-            // 썸네일 설정
-            List<String> thumbnails = (List<String>) datums.source.get(HummingUtils.ElasticField.THUMBNAILS);
-            videoListFragment.setThumbnails(thumbnails);
-
-
-            changeIds = true;
-
-            Handler handlers2 = new Handler();
-            handlers2.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    if(draggableView.isMaximized()){
-                        if(changeIds){
-                            changeIds = false;
-                            videoListFragment.changeView();
-                        }
-                    }
-                    postClick(MainActivity.SEARCH_IDS_VALUE);
-                }
-            }, 100);
-        }*/
-
-    }
     public void setYoutubeId(String youtubeId, int seek) {
         /*if(getApplicationInstance().getConfig().getVideoMode().equals("youtube")){
             draggablePanel.minimize();
