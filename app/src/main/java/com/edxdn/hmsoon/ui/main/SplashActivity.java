6bac6f76241e7d81980f0619e20418f306e1082c
peterkimlab <niceguy0825@hotmail.com>
2018-11-26 08:32:21 +0900

    splah is added. 3

--- app/src/main/java/com/edxdn/hmsoon/ui/main/SplashActivity.java
+++ app/src/main/java/com/edxdn/hmsoon/ui/main/SplashActivity.java
@@ -2,13 +2,26 @@ package com.edxdn.hmsoon.ui.main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+
+import com.exam.english.R;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 1000);
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
